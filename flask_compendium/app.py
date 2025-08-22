df875fcaa4a4cb7ac89cf8423744ff95f4de9ca2
Paradox60_Pad <webaz60@gmail.com>
2025-08-22 15:18:55 +0300

    reworked full structure of the project added new application js for js lessons. Js gonna build with jinja2 templates

--- flask_compendium/app.py
+++ flask_compendium/app.py
@@ -4,28 +4,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("CSS_temp/index.html")
 
 @app.route("/selector_page")
 def text_page():
-    return render_template("selector.html")
+    return render_template("CSS_temp/selector.html")
 
 @app.route("/blocks")
 def block_page():
-    return render_template("blocks.html")
+    return render_template("CSS_temp/blocks.html")
 
 @app.route("/flex")
 def flex():
-    return render_template("flex.html")
+    return render_template("CSS_temp/flex.html")
 
 @app.route("/grid")
 def grid():
-    return render_template("grid.html")    
+    return render_template("CSS_temp/grid.html")    
 
 @app.route("/position")
 def position():
-    return render_template("position.html") 
+    return render_template("CSS_temp/position.html") 
 
 @app.route("/inline")
 def inline():
-    return render_template("inline.html") 
+    return render_template("CSS_temp/inline.html") 
+
+@app.route("/js_home")
+def js_start():
+    return render_template("JS_temp/js_index.html") 
